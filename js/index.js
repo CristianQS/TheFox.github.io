59adb1cd44405627a2ccf1d307d456faf9559213
Cristian <cristianqs31@gmail.com>
2018-03-29 23:52:17 +0100

    nav responsive

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function dropdown(){
+    if($('.collapse').hasClass("active")){
+        $('.collapse').removeClass("active");
+    }else{
+        $('.collapse').addClass("active");
+    }
+    console.log("hola");
+}
+ 
