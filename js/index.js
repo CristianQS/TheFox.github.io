b5021f735a76dfc8fae5cef6e15bc85df769216f
Cristian <cristianqs31@gmail.com>
2018-03-30 18:29:17 +0100

    Responsive Done

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-function dropdown(){
+function dropdownNav(){
     if($('.collapse').hasClass("active")){
         $('.collapse').removeClass("active");
     }else{
         $('.collapse').addClass("active");
     }
     console.log("hola");
+}
+
+function dropdown(id) {
+    $(".span--dropdown p").hide();
+    $(".wrapper__image--dropdown img").hide();
+    if($(id).is("#Creative")){
+        $(".wrapper__image--dropdown img").show();
+    }
+    $(id).show();
 }
  
